df6063b2c0af9bae185290f177fa9dbdf0c4190f
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2018-01-13 22:46:54 -0500

    Monitor updates
    Updated for new monitor layout (all Display Port)
    
    Added monitor assignments for workspaces

--- .config/i3/scripts/termite.sh
+++ .config/i3/scripts/termite.sh
@@ -3,4 +3,3 @@
 (termite --title=Matrix -e 'zsh -c "cmatrix -BC blue && zsh"' --hold &)
 (termite --title=Neofetch -e 'zsh -c "neofetch"' --hold &)
 (termite --title=Terminal &)
-(termite --title=Terminal &)
